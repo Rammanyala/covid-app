5c40c36e6714ad152275e4c83e9a3880e8dd8713
ram <iamrammanyala@gmail.com>
2020-07-11 01:36:57 +0530

    Updates

---
+++ precache-manifest.f83cbfb1c3bc809dc4c29f8384181604.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3aa8056784ad6ab459d4",
+    "url": "/covid-app/static/js/runtime~main.1092116a.js"
+  },
+  {
+    "revision": "064ab32d2029bbfb4bae",
+    "url": "/covid-app/static/js/main.497dc2e5.chunk.js"
+  },
+  {
+    "revision": "2ec073e5291cf9379117",
+    "url": "/covid-app/static/js/2.1dbae479.chunk.js"
+  },
+  {
+    "revision": "064ab32d2029bbfb4bae",
+    "url": "/covid-app/static/css/main.f5de8a8d.chunk.css"
+  },
+  {
+    "revision": "1d7c46ec6c29e272a57f64f2bf45985a",
+    "url": "/covid-app/index.html"
+  }
+];
